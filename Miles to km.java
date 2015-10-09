909e647460581dcf8f32b1e493733589115d478d
nxman3 <phong.pham@edu.sait.ca>
2015-10-08 20:05:54 -0600

    adding comments

--- Miles to km.java
+++ Miles to km.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
+import java.util.Scanner;//Importing Ultility Scanner
 
 
 public class Assignment1
 	{
-	static Scanner sc=new Scanner(System.in);
-	static final double CONVERT_MILES=1.609;
+	static Scanner sc=new Scanner(System.in);//Scanner
+	static final double CONVERT_MILES=1.609;//Constant value
 	public static void main(String[]args)
-		{System.out.print("Enter number of miles: ");
-		double numMile=sc.nextDouble();
-		double numMileKilo=numMile*CONVERT_MILES;
-		System.out.printf("%.3f miles convert to %.3f kilometers", numMile, numMileKilo);
+		{System.out.print("Enter number of miles: ");//command line prints 
+		double numMile=sc.nextDouble();//Scans for user input
+		double numMileKilo=numMile*CONVERT_MILES;//formula to convert miles to km
+		System.out.printf("%.3f miles convert to %.3f kilometers", numMile, numMileKilo); //program displays miles to km
 		
 		}
 	}
